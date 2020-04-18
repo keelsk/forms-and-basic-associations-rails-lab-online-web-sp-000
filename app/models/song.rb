957095eb5a1e309f75912bc316a1166232b70667
Kirsten Keels <keelsk@gmail.com>
2020-04-18 08:55:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,22 +12,22 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def note_ids=(ids)
-    ids.each do |id|
-      note = Note.find_by_id(id)
-      self.notes << note
-    end
-  end
-
-  def genre_id=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name
-    self.genre.name
-  end
+  # def note_ids=(ids)
+  #   ids.each do |id|
+  #     note = Note.find_by_id(id)
+  #     self.notes << note
+  #   end
+  # end
+  # 
+  # def genre_id=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
+  # 
+  # def genre_name=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
+  # 
+  # def genre_name
+  #   self.genre.name
+  # end
 end
