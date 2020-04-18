359c9a27c3317138a0482239959742c02bc3ce78
Kirsten Keels <keelsk@gmail.com>
2020-04-18 08:48:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -23,6 +23,10 @@ class Song < ActiveRecord::Base
     self.genre = Genre.find_or_create_by(name: name)
   end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
   def genre_name
     self.genre.name
   end
